c7c963caa9bc143f71181b057dc881f7609d7379
Ayagari Mukund <mukund1403@yahoo.com>
2026-01-27 13:43:30 +0800

    full esp32 comms code

---
+++ FruitNinjaESP32/secrets.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// WiFi
+const char *ssid = ""; // Enter your home wifis username
+const char *password = ""; // Enter your home wifis password
+
+// MQTT
+const char *mqtt_broker_home = "192.168.1.201";
+const char *mqtt_broker_school = "192.168.x.x"; // fill in later
+const int mqtt_port = 1883;
+const char *mqtt_username = "esp32";
+const char *mqtt_password = "capstone";
--- ultra96_project/main.py
+++ ultra96_project/main.py
@@ -1,6 +1,11 @@
 import paho.mqtt.client as mqtt
 from driver import MockPYNQDriver  # swap with real PYNQDriver later
 import json
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Initialize driver
 driver = MockPYNQDriver()
@@ -8,14 +13,15 @@ driver = MockPYNQDriver()
 # MQTT configuration
 BROKER_HOST = "localhost"
 BROKER_PORT = 1883  # Reverse tunnel port
-SENSOR_TOPIC = "imu/window/#"
-GESTURE_TOPIC = "gesture/detected"
+SENSOR_TOPIC = "fruitninja/imu/window"
+GESTURE_TOPIC = "fruitninja/gesture/detected"
+
 
 def parse_sensor_payload(payload):
     """
     Need to edit this for AI!
     Convert raw MQTT payload to input for driver.run().
-    Replace with actual parsing logic (e.g., NumPy array of sensor data)
+    Replace with actual parsing logic (e.g. np array of sensor data)
     """
     try:
         data = json.loads(payload.decode("utf-8"))
@@ -23,14 +29,22 @@ def parse_sensor_payload(payload):
         data = payload  # fallback for mock
     return data
 
-def on_message(client, userdata, msg):
+def on_message(client:mqtt.Client, userdata, msg):
     sensor_data = parse_sensor_payload(msg.payload)
     gesture = driver.run(sensor_data)
-    client.publish(GESTURE_TOPIC, gesture)
+    
+    gesture_msg = {
+        "gesture": gesture,
+        "confidence": 0.93  # placeholder
+    }   
+
+    client.publish(GESTURE_TOPIC, json.dumps(gesture_msg), qos=1)
+
     print(f"[MQTT] Published gesture: {gesture}")
 
 def main():
-    client = mqtt.Client()
+    client = mqtt.Client(client_id=os.getenv("ULTRA96_USERNAME"))
+    client.username_pw_set(username=os.getenv("ULTRA96_USERNAME"), password=os.getenv("ULTRA96_PASSWORD"))
     client.on_message = on_message
     client.connect(BROKER_HOST, BROKER_PORT)
     client.subscribe(SENSOR_TOPIC)
