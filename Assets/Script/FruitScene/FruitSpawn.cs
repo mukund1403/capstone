5930b7222d6240ba84fea57a4a10bb4ea0679c3d
fxe025 <110606445+fxe025@users.noreply.github.com>
2026-02-08 01:55:01 +0800

    Implement Fruit Spawn
    Add ThrowFromBottom()
    Triggered by Button OnClick()

--- Assets/Script/FruitScene/FruitSpawn.cs
+++ Assets/Script/FruitScene/FruitSpawn.cs
@@ -1,11 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class FruitSpawn : MonoBehaviour
 {
-    private void DropFromTop()
+    private float lowestYPoint = -5;
+
+    public float spawnRate = 4;
+    private float timer;
+
+    private bool isActive;
+
+    [SerializeField] private GameObject lemonPrefab, pearPrefab, strawberryPrefab, applePrefab, watermelonPrefab, peachPrefab;
+    private GameObject[] fruits;
+
+    private enum Direction
     {
-        
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isActive = false;
+        timer = 0;
+        fruits = new GameObject[]
+        {
+            lemonPrefab, 
+            pearPrefab, 
+            strawberryPrefab, 
+            applePrefab, 
+            watermelonPrefab, 
+            peachPrefab
+        };
+    }
+
+    public void setSpawnOnClick()
+    {
+        if (isActive)
+        {
+            isActive = false;
+        }
+        else
+        {
+            isActive = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            if (timer < spawnRate)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                ThrowFromBottom();
+                timer = 0;
+            }
+        }
+    }
+
+    private void applyPhysics(GameObject item, float force, Direction dir)
+    {
+        Rigidbody rb = item.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        float spinForce = 5;
+        switch (dir)
+        {
+            case Direction.Left:
+                rb.AddForce(Vector3.left * force, ForceMode.Impulse);
+                rb.AddTorque(Random.insideUnitSphere * spinForce, ForceMode.Impulse);
+                break;
+            case Direction.Right:
+                rb.AddForce(Vector3.right * force, ForceMode.Impulse);
+                rb.AddTorque(Random.insideUnitSphere * spinForce, ForceMode.Impulse);
+                break;
+            case Direction.Up:
+                rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+                rb.AddTorque(Random.insideUnitSphere * spinForce, ForceMode.Impulse);
+                break;
+        }
+    }
+
+    private void ThrowFromBottom()
+    {
+        float force = 11;
+        GameObject fruitChosen = fruits[Random.Range(0, fruits.Length)];
+        if (fruitChosen != null)
+        {
+            GameObject spawnedFruit = Instantiate(fruitChosen, new Vector3(Random.Range(-3, 3), -5, 5), transform.rotation);
+            spawnedFruit.SetActive(true);
+            applyPhysics(spawnedFruit, force, Direction.Up);
+            //GameObject spawnedFruit = Instantiate(fruitChosen, new Vector3(Random.Range(-1, 1), 0, 5), transform.rotation);
+            //spawnedFruit.SetActive(true);
+        }
+    }
+
+    private void ThrowFromLeft()
+    {
+
+    }
+
+    private void ThrowFromRight()
+    {
     }
 }
--- Assets/Script/FruitScene/PrefabCreatorImage.cs
+++ Assets/Script/FruitScene/PrefabCreatorImage.cs
@@ -1,18 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using TMPro;
 
 
 public class PrefabCreatorImage : MonoBehaviour
 {
-    [SerializeField] private GameObject lemonPrefab, pearPrefab, strawberryPrefab, applePrefab, watermelonPrefab, peachPrefab;
     [SerializeField] private Vector3 prefabOffset;
+    private GameObject contentRoot;
+    private bool isInitialized;
 
-    private GameObject lemon, pear, strawberry, apple, watermelon, peach;
     private ARTrackedImageManager aRTrackedImageManager;
 
     [SerializeField] private GameObject katanaPrefab;
+    [SerializeField] private TMP_Text text;
     private GameObject katana;
 
     private void OnEnable()
@@ -21,26 +24,59 @@ public class PrefabCreatorImage : MonoBehaviour
         aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
     }
 
+    private void SetMessage(string message)
+    {
+        text.text = message;
+    }
+
     private void OnImageChanged(ARTrackedImagesChangedEventArgs obj)
     {
         foreach (ARTrackedImage image in obj.added)
         {
-            //CreateFruit(lemon, image, lemonPrefab);
-            //CreateFruit(pear, image, pearPrefab);
-            //CreateFruit(strawberry, image, strawberryPrefab);
-            //CreateFruit(apple, image, applePrefab);
-            //CreateFruit(watermelon, image, watermelonPrefab);
-            //CreateFruit(peach, image, peachPrefab);
-            katana = Instantiate(katanaPrefab, image.transform);
-            katana.SetActive(true);
+            contentRoot = new GameObject("ContentRoot");
+            contentRoot.transform.SetParent(image.transform, false);
+            katana = Instantiate(katanaPrefab, contentRoot.transform);
+            isInitialized = false;
+        }
+        foreach (ARTrackedImage image in obj.updated)
+        {
+            if (katana == null)
+            {
+                SetMessage("Kanata Status: not exist");
+                return;
+            }
+            if (image.trackingState == TrackingState.Tracking)
+            {
+                katana.SetActive(true);
+                if (!isInitialized)
+                {
+                    InitializeContent();
+                    isInitialized = true;
+                }
+
+                string message = "Kanata Status: tracking active\n" + katana.transform.position;
+                message += "\n";
+                message += image.transform.position;
+                SetMessage(message);
+            }
+            else
+            {
+                katana.SetActive(false);
+                SetMessage("Kanata Status: tracking inactive");
+            }
         }
     }
 
-    private void CreateFruit(GameObject fruit, ARTrackedImage image, GameObject fruitPrefab)
+    private void InitializeContent()
     {
-        fruit = Instantiate(fruitPrefab, image.transform);
-        fruit.SetActive(true);
-        //fruit.SetActive(image.trackingState == TrackingState.Tracking);
-        //fruit.transform.SetParent(image.transform, false);
+        contentRoot.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        //contentRoot.transform.localPosition += prefabOffset;
+        contentRoot.transform.localRotation = Quaternion.Euler(0, 90, 0);
+
+        foreach (var r in contentRoot.GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = false;
+            r.enabled = true;
+        }
     }
 }
